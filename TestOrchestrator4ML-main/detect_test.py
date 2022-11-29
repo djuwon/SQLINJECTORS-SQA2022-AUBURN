0ee97a136be9b9f966dcafb89c69b03ba52fa671
David <djung@uda1.com>
2022-11-28 19:47:21 -0600

    test

--- TestOrchestrator4ML-main/detect_test.py
+++ TestOrchestrator4ML-main/detect_test.py
@@ -15,7 +15,7 @@ def giveTimeStamp():
     
 def checkTestFile(path2dir): 
     """
-    Check sif project includes tests. Making Change
+    Check sif project includes tests. Making Changes
     We look for files that start with test (or tests, Test, Tests) or end with test.py (tests.py,
     Test.py, Tests.py).
     """
